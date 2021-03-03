6036a24350c93c755b116836f71bf6be325de0a4
lffelmann <67123057+lffelmann@users.noreply.github.com>
2021-03-03 23:58:23 +0100

    made main prg to func argparse

--- svm.py
+++ svm.py
@@ -1,6 +1,7 @@
 """
 Program to create and save a new Support Vector Machine.
 """
+from typing import Any
 
 from sklearn import svm
 from sklearn.metrics import classification_report
@@ -8,8 +9,19 @@ from sklearn.model_selection import train_test_split
 
 import coincount_lib as ccl
 
-if __name__ == '__main__':
+def svm(arg: Any = None) -> None:
+    """
+    Program to create and save a new Support Vector Machine.
+
+    Parameters:
+        arg (Any): Arguments from argparse.
+    """
     try:
+        if arg is None:
+            model_save = 'svm/model.sav'
+        else:
+            model_save = arg.model
+
         data_target = ccl.svm_readcsv('./svm/csvfiles/1cnts.csv', 1)
         data_target = ccl.svm_readcsv('./svm/csvfiles/2cnts.csv', 2, data_target[0], data_target[1])
         data_target = ccl.svm_readcsv('./svm/csvfiles/5cnts.csv', 5, data_target[0], data_target[1])
@@ -27,6 +39,9 @@ if __name__ == '__main__':
 
         print(classification_report(y_test, y_pred))  # classification report
 
-        ccl.svm_writemodel('svm/model.sav', model)  # safe model
+        ccl.svm_writemodel(model_save, model)  # safe model
     except:
         raise
+
+if __name__ == '__main__':
+    svm()
