e52e6236a6f6662d131228cc8f93684da7ed774a
lffelmann <67123057+lffelmann@users.noreply.github.com>
2021-03-03 23:20:33 +0100

    optimized code

--- coincount_lib.py
+++ coincount_lib.py
@@ -2,14 +2,16 @@
 Library with all the functions for CoinCount!
 """
 
-import cv2
-from math import pi, sqrt
-import numpy as np
 import csv
 import pickle
+from math import pi, sqrt
 from typing import Any
 
-def contour(image: Any, blur: int=3, threshold: int=110) -> list:
+import cv2
+import numpy as np
+
+
+def contour(image: Any, blur: int = 3, threshold: int = 110) -> list:
     """
     Returns the contours of object in a image.
 
@@ -26,12 +28,13 @@ def contour(image: Any, blur: int=3, threshold: int=110) -> list:
         img_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)  # to gray
         img_blur = cv2.GaussianBlur(img_gray, (blur, blur), 0)  # blur
         img_bnry = cv2.threshold(img_blur, threshold, 255, cv2.THRESH_BINARY_INV)[1]  # binary
-        cnts, _ = cv2.findContours(img_bnry, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # get contours
+        cnts, _ = cv2.findContours(img_bnry, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # get contours
         return [cnts, img_gray, img_blur, img_bnry]
     except:
         raise
 
-def circle(contour: list, image: Any, accuracy: float=0.15, min_area: float=0) -> list:
+
+def circle(contour: list, image: Any, accuracy: float = 0.15, min_area: float = 0) -> list:
     """
     Return if a contour is a circle or not.
 
@@ -49,11 +52,12 @@ def circle(contour: list, image: Any, accuracy: float=0.15, min_area: float=0) -
 
     """
     try:
-        area = cv2.contourArea(contour) # area
-        perimeter = cv2.arcLength(contour, True) # perimeter
-        radius_area = sqrt(area/pi)     # radius from area
-        radius_perimeter = perimeter / (2 * pi) # radius form perimeter
-        if (radius_area * (1-accuracy)) <= radius_perimeter <= (radius_area * (1+accuracy)) and area >= min_area: # compare radius area perimeter
+        area = cv2.contourArea(contour)  # area
+        perimeter = cv2.arcLength(contour, True)  # perimeter
+        radius_area = sqrt(area / pi)  # radius from area
+        radius_perimeter = perimeter / (2 * pi)  # radius form perimeter
+        if (radius_area * (1 - accuracy)) <= radius_perimeter <= (
+                radius_area * (1 + accuracy)) and area >= min_area:  # compare radius area perimeter
             mask = np.zeros(image.shape[:2], dtype="uint8")
             cv2.drawContours(mask, [contour], -1, 255, -1)
             masked = cv2.bitwise_and(image, image, mask=mask)
@@ -64,7 +68,8 @@ def circle(contour: list, image: Any, accuracy: float=0.15, min_area: float=0) -
     except:
         raise
 
-def rectangle(contour: list, accuracy: float=0.01, min_area: float=0) -> list:
+
+def rectangle(contour: list, accuracy: float = 0.01, min_area: float = 0) -> list:
     """
     Return if a contour is a rectangle or not.
 
@@ -80,9 +85,9 @@ def rectangle(contour: list, accuracy: float=0.01, min_area: float=0) -> list:
     """
     try:
         length = []
-        area = cv2.contourArea(contour) # area
-        approx = cv2.approxPolyDP(contour, accuracy * cv2.arcLength(contour, True), True) # simplify contour
-        if (len(approx) == 4) and area >= min_area:    # if four sides -> return length
+        area = cv2.contourArea(contour)  # area
+        approx = cv2.approxPolyDP(contour, accuracy * cv2.arcLength(contour, True), True)  # simplify contour
+        if (len(approx) == 4) and area >= min_area:  # if four sides -> return length
             for i in range(0, len(approx) - 1):
                 point = approx[i] - approx[i + 1]  # x and y form 0,0
                 point = np.ndarray.tolist(point[0])  # point to array
@@ -105,6 +110,7 @@ def rectangle(contour: list, accuracy: float=0.01, min_area: float=0) -> list:
     except:
         raise
 
+
 def len_pixel(reference_mm: list, reference_pxl: list) -> float:
     """
     Returns the length of a pixel in mm, when there is a reference rectangle.
@@ -117,15 +123,16 @@ def len_pixel(reference_mm: list, reference_pxl: list) -> float:
         (float): Length of a pixel in mm.
     """
     try:
-        reference_mm.sort()    # sort list reference_val
-        reference_pxl.sort()    # sort list reference_pxl
-        pxl_len_0 = reference_mm[0] / reference_pxl[0]   # calc len of pxl on short side
-        pxl_len_1 = reference_mm[1] / reference_pxl[1]   # calc len of pxl on long side
+        reference_mm.sort()  # sort list reference_val
+        reference_pxl.sort()  # sort list reference_pxl
+        pxl_len_0 = reference_mm[0] / reference_pxl[0]  # calc len of pxl on short side
+        pxl_len_1 = reference_mm[1] / reference_pxl[1]  # calc len of pxl on long side
         return (pxl_len_0 + pxl_len_1) / 2  # calc and return average len
     except:
         raise
 
-def convert_len(length_of_pixel:float, length_in_pixel:float) -> float:
+
+def convert_len(length_of_pixel: float, length_in_pixel: float) -> float:
     """
     Returns a length of pixel in mm.
 
@@ -137,12 +144,13 @@ def convert_len(length_of_pixel:float, length_in_pixel:float) -> float:
         (float): Length of the element in pixel.
     """
     try:
-        actual_length = length_of_pixel * length_in_pixel   # calc actual len
+        actual_length = length_of_pixel * length_in_pixel  # calc actual len
         return actual_length
     except:
         raise
 
-def svm_readcsv(path: str, target_val: Any, list_data: list=None, list_target: list=None) -> list:
+
+def svm_readcsv(path: str, target_val: Any, list_data: list = None, list_target: list = None) -> list:
     """
     Reads data from csv to list and adds target to list.
     
@@ -158,27 +166,28 @@ def svm_readcsv(path: str, target_val: Any, list_data: list=None, list_target: l
         (list): List with list of csv values and list of targets -> [[Csv], [Target]]
     """
     try:
-        if list_data is None:   # if no data list -> crate data list
+        if list_data is None:  # if no data list -> crate data list
             data = []
         else:
             data = list_data
-        if list_target is None: # if no target list -> crate target list
+        if list_target is None:  # if no target list -> crate target list
             target = []
         else:
             target = list_target
 
         file = open(path, 'r', newline='')  # open file
-        reader = csv.reader(file)   # create reader
+        reader = csv.reader(file)  # create reader
 
         for row in reader:  # add csv and target to list
             data.append(row)
             target.append(target_val)
 
-        file.close()    # close file
+        file.close()  # close file
 
         return [data, target]
     except:
         raise
+
 
 def svm_writecsv(path: str, values: list) -> None:
     """
@@ -192,11 +201,12 @@ def svm_writecsv(path: str, values: list) -> None:
     """
     try:
         file = open(path, 'a', newline='')  # open file
-        writer = csv.writer(file)   # create writer
-        writer.writerows(values)    # write values
-        file.close()    # close file
+        writer = csv.writer(file)  # create writer
+        writer.writerows(values)  # write values
+        file.close()  # close file
     except:
         raise
+
 
 def svm_writemodel(path: str, model: Any) -> None:
     """
@@ -207,11 +217,12 @@ def svm_writemodel(path: str, model: Any) -> None:
         model (Any): Model to save.
     """
     try:
-        file = open(path, 'wb') # open file
-        pickle.dump(model, file)    # save file
-        file.close()    # close file
+        file = open(path, 'wb')  # open file
+        pickle.dump(model, file)  # save file
+        file.close()  # close file
     except:
         raise
+
 
 def svm_readmodel(path: str) -> Any:
     """
@@ -224,9 +235,9 @@ def svm_readmodel(path: str) -> Any:
         (Any): Saved model.
     """
     try:
-        file = open(path, 'rb') # open file
-        model = pickle.load(file)   # load model
-        file.close()    # close file
+        file = open(path, 'rb')  # open file
+        model = pickle.load(file)  # load model
+        file.close()  # close file
         return model
     except:
         raise
