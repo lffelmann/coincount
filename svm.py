47d5471781ef57410568b55e7654280b45b3007e
lffelmann <67123057+lffelmann@users.noreply.github.com>
2021-03-05 09:57:15 +0100

    split 0.33 -> 0.25

--- svm.py
+++ svm.py
@@ -30,7 +30,7 @@ def svm(arg: Any = None) -> None:
         data_target = ccl.svm_readcsv('./svm/csvfiles/20cnts.csv', 20, data_target[0], data_target[1])
 
         X_train, X_test, y_train, y_test = train_test_split(data_target[0], data_target[1],
-                                                            test_size=0.33)  # split dataset
+                                                            test_size=0.25)  # split dataset
 
         model = supvecmac.SVC(kernel='linear')  # create model
 
